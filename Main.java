3243427888014353e6d56167f9a667285e48f32e
wu3976 <54728465+wu3976@users.noreply.github.com>
2019-08-30 19:57:04 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package osu.cse2221;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        PlannerFrame planner = new PlannerFrame();
+        planner.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        planner.setVisible(true);
+    }
+}
